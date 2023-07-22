6173650f2e2d2a8832b194442c6ae81e088948f0
Goodl3oyZ <panudetzx2@gmail.com>
2023-07-22 17:37:25 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -10,16 +10,21 @@ import { useState } from "react";
 export default function Home() {
   //tasks = array of {id: string, title: string, completed: boolean}
   const [tasks, setTasks] = useState([]);
+  const [allTask, setAllTask] = useState(0);
+  const [done, setDone] = useState(0);
 
   const addTask = (newTaskTitle) => {
     const newTask = { id: nanoid(), title: newTaskTitle, completed: false };
     const newTasks = [...tasks, newTask];
     setTasks(newTasks);
+    setAllTask(allTask + 1);
   };
 
   const deleteTask = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(newTasks);
+    setAllTask(allTask - 1);
+    setDone(newTasks.filter((x) => x.completed).length);
   };
 
   const toggleDoneTask = (taskId) => {
@@ -30,6 +35,7 @@ export default function Home() {
     const task = newTasks.find((x) => x.id === taskId);
     task.completed = !task.completed;
     setTasks(newTasks);
+    setDone(newTasks.filter((x) => x.completed).length);
   };
 
   return (
@@ -41,7 +47,7 @@ export default function Home() {
       <div style={{ maxWidth: "400px" }} className="mx-auto">
         {/* Task summary */}
         <p className="text-center text-secondary fst-italic">
-          All (...) Done (...)
+          All ({allTask}) Done ({done})
         </p>
         {/* task input */}
         <TaskInput addTaskFunc={addTask} />
@@ -60,7 +66,11 @@ export default function Home() {
       </div>
 
       {/* //footer section */}
-      <Footer year="2023" fullName="Chayanin Suatap" studentId="12345678" />
+      <Footer
+        year="2023"
+        fullName="Phanudet Sueaphueak"
+        studentId="650610797"
+      />
     </div>
   );
 }
